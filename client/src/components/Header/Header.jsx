c47135bbcc02d0f41edd871c0833f363b8ce7194
Anatoli Pavlov <18app81@gmail.com>
2023-12-11 18:20:00 +0200

    Update Header.jsx

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,22 +1,24 @@
+import { Link } from "react-router-dom";
+
 export default function Header() {
     return (
         <header>
             {/* Navigation */}
-            <h1><a className="home" href="#">GamesPlay</a></h1>
+            <h1><Link to={"/"} className="home">GamesPlay</Link></h1>
             <nav>
-                <a href="#">All games</a>
+                <Link to={"/games"}>All Games</Link>
                 {/* Logged-in users */}
                 <div id="user">
-                    <a href="#">Create Game</a>
-                    <a href="#">Logout</a>
-                </div>
+                    <Link to={"/create-game"}>Create Game</Link>
+                    <Link to={"/logout"}>Logout</Link>
+                </div >
                 {/* Guest users */}
-                <div id="guest">
-                    <a href="#">Login</a>
-                    <a href="#">Register</a>
-                </div>
-            </nav>
-        </header>
+                < div id="guest" >
+                    <Link to={"/login"}>Login</Link>
+                    <Link to={"/register"}>Register</Link>
+                </div >
+            </nav >
+        </header >
 
     );
 }
