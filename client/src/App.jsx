8274e1a7b2a980c4aa6b78d0c7f3e4d6cdad1442
Anatoli Pavlov <18app81@gmail.com>
2023-12-11 18:20:05 +0200

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import Header from "./components/Header/Header"
 import Home from "./components/Home/Home"
+import GameList from './components/GameList/GameList';
 
 function App() {
 
@@ -11,12 +12,13 @@ function App() {
 
             <Routes>
                 <Route path='/' element={<Home />} />
+                <Route path='/games' element={<GameList />} />
             </Routes>
 
 
 
         </div>
-    )
+    );
 }
 
 export default App
