1743e61cda0280a1e2fc926763a6ae5e81c52ea1
Anatoli Pavlov <18app81@gmail.com>
2023-12-13 16:33:46 +0200

    Add - Game Details page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import GameList from './components/GameList/GameList';
 import GameCreate from './components/GameCreate/GameCreate';
 import Login from './components/Login/Login';
 import Register from './components/Register/Register';
+import GameDetails from './components/GameDetails/GameDetails';
 
 function App() {
 
@@ -19,6 +20,7 @@ function App() {
                 <Route path='/create-game' element={<GameCreate />} />
                 <Route path='/login' element={<Login />} />
                 <Route path='/register' element={<Register />} />
+                <Route path='/games/:gameId' element={<GameDetails />} />
             </Routes>
 
 
