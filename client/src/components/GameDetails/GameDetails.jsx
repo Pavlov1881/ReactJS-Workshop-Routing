60f7d23b25664530e01eb8055da5e5802ef565b5
Anatoli Pavlov <18app81@gmail.com>
2023-12-13 16:41:05 +0200

    Update GameDetails.jsx

--- client/src/components/GameDetails/GameDetails.jsx
+++ client/src/components/GameDetails/GameDetails.jsx
@@ -20,7 +20,7 @@ export default function GameDetails() {
             <div className="info-section">
                 <div className="game-header">
                     <img className="game-img" src={game.imageUrl} />
-                    <h1>Bright</h1>
+                    <h1>{game.title}</h1>
                     <span className="levels">MaxLevel: {game.maxLevel}</span>
                     <p className="type">{game.category}</p>
                 </div>
