c8e6881c2077f4eca2319a3d467923ec8a1a0acb
Anatoli Pavlov <18app81@gmail.com>
2023-12-13 11:46:26 +0200

    typo fix

--- client/src/components/GameList/GameList.jsx
+++ client/src/components/GameList/GameList.jsx
@@ -1,27 +1,34 @@
-import { useEffect, useState } from 'react';
-
-import * as gameService from '../../services/gameService';
-import GameListItem from './game-list-item/GameListItem';
+import { useEffect, useState } from "react";
+import * as gameServise from '../../services/gameService';
+import GameListItem from "./GameListItem/GameListItem";
 
 export default function GameList() {
+
     const [games, setGames] = useState([]);
 
     useEffect(() => {
-        gameService.getAll()
-            .then(result => setGames(result));
+
+        gameServise.getAll()
+            .then(result => setGames(result))
+
     }, []);
 
     return (
         <section id="catalog-page">
             <h1>All Games</h1>
+            {/* Display div: with information about every game (if any) */}
 
-            {games.map(game => (
-                <GameListItem key={game._id} {...game} />
-            ))}
+            {
+                games.map(game => (
+                    <GameListItem key={game._id} {...game} />
+                ))
+            }
 
+            {/* Display paragraph: If there is no games  */}
             {games.length === 0 && (
                 <h3 className="no-articles">No articles yet</h3>
             )}
         </section>
+
     );
 }
